24733ec547b9d1e29c6fad7fc89d0c65e1f0622d
ritik <ritikbhatt020@gmail.com>
2023-11-27 11:09:24 +0530

    Changed Dockerfile

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/SpringBootTutorialApplication.java
@@ -1,9 +1,17 @@
 package com.dailycodebuffer.Springboot.tutorial;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootTutorialApplication {
 
 	public static void main(String[] args) {
